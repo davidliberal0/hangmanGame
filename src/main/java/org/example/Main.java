adefe5e7598c658200788485e0710b580d36abbe
David Liberal <davidliberal0@gmail.com>
2025-12-19 17:41:40 -0500

    set up initial hangman word state

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        String word = "pizza";
+        int wrongGuesses = 0;
 
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            wordState.add('_');
+        }
+
+        System.out.println(wordState);
+
+        scanner.close();
     }
 }
