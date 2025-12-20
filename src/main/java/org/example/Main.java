43aad364e5d703fa625ddc31b6306fd774c8af81
David Liberal <davidliberal0@gmail.com>
2025-12-19 23:19:03 -0500

    setup flow and drawing hangman for wrong guesses

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        String word = "pizza";
+        String word = "watermelon";
         int wrongGuesses = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -17,8 +17,54 @@ public class Main {
             wordState.add('_');
         }
 
-        System.out.println(wordState);
+        System.out.println("************************");
+        System.out.println("Welcome to Java Hangman!");
+        System.out.println("************************");
+
+        System.out.println(getHangmanArt(2));
 
         scanner.close();
+    }
+
+    static String getHangmanArt(int wrongGuesses) {
+        return switch(wrongGuesses) {
+            case 0 -> """
+                    
+                    
+                    
+                      """;
+            case 1 -> """
+                        o
+                      
+                      
+                      """;
+            case 2 -> """
+                        o
+                        |
+                    
+                      """;
+            case 3 -> """
+                        o
+                       /|
+                       
+                      """;
+            case 4 -> """
+                        o
+                       /|\\
+                                            
+                      """;
+            case 5 -> """
+                        o
+                       /|\\
+                       /                  
+                      """;
+            case 6 -> """
+                        o
+                       /|\\
+                       / \\                     
+                      """;
+            default ->
+                "Game over";
+        };
     }
 }
